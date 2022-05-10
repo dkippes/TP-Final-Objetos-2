488e133d9c66a9dfb821f2219de5d8df65abf0f4
dkippes <kippes.diego@gmail.com>
2022-05-09 23:19:54 -0300

    setup proyecto final

---
+++ test/a/la/caza/de/las/vinchucas/TestPrueba.java
@@ -0,0 +1,16 @@
+package a.la.caza.de.las.vinchucas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPrueba {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
