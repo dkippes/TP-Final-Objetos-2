d88d2eceeca89affdce7a4d3d5ffe5c01ad439d0
scamipia <camila.pia.scaglioni@gmail.com>
2022-06-09 19:15:50 -0300

    binaryFilter arreglado

--- src/a/la/caza/de/las/vinchucas/search/BinaryFilter.java
+++ src/a/la/caza/de/las/vinchucas/search/BinaryFilter.java
@@ -9,10 +9,7 @@ public abstract class BinaryFilter implements IFilter {
 		private IFilter filter1;
 		private IFilter filter2;
 
-		public void setFilters(IFilter filter1, IFilter filter2) {
-			this.filter1 = filter1;
-			this.filter2 = filter2;
-		}
+		public abstract void setFilters(IFilter filter1, IFilter filter2);
 
 		abstract public List<Sample> searchSamples(List<Sample> allSamples);
 	
